4a69f6cc01ff774a52aeec47c3e549d0c04c3ef6
Starship100 <jimmy.widlund@outlook.com>
2026-02-14 00:32:11 +0100

    list

--- lists.py
+++ lists.py
@@ -5,15 +5,25 @@ cars.sort()
 print(cars)
 cars.sort(reverse=True)
 print(cars)
-
+numbers = [28, 9, 100, 55]
+print(min(numbers))
+print(max(numbers))
+print(sum(numbers))
 print(cars[0], cars[1], cars[2], cars[3])
 print(cars[0], friends[0], shows[1])
 print(friends[0])
-""""
+
 friends.sort()
 print(friends)
 cars.sort()
 print(cars)
+shows.append("Arkiv X")
+shows.remove("Handmaids Tale")
 shows.sort()
 print(shows)
-"""
+cars = ["Kia Sportage", "Scoda Octavia", "Hyundai", "Mazda 626"]
+print(cars)
+cars.clear()
+print(cars)
+del cars
+#print(cars)
