659f08d7d996a02eebe618c0f748da53a2bebe6f
Starship100 <jimmy.widlund@outlook.com>
2026-02-13 23:52:38 +0100

    list

---
+++ lists.py
@@ -0,0 +1,19 @@
+cars = ["Kia Sportage", "Scoda Octavia", "Hyundai", "Mazda 626"]
+friends = ["♥Sara♥", "Johan", "Anders", "Micke"]
+shows = ["The Office", "The Walking Dead", "Handmaids Tale"]
+cars.sort()
+print(cars)
+cars.sort(reverse=True)
+print(cars)
+
+print(cars[0], cars[1], cars[2], cars[3])
+print(cars[0], friends[0], shows[1])
+print(friends[0])
+""""
+friends.sort()
+print(friends)
+cars.sort()
+print(cars)
+shows.sort()
+print(shows)
+"""
