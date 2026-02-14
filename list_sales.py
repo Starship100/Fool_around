5d382d461df56ef4e7d0717b6c261081119b115c
Starship100 <jimmy.widlund@outlook.com>
2026-02-14 21:27:40 +0100

    list_sales

--- list_sales.py
+++ list_sales.py
@@ -5,4 +5,12 @@ sales = []
 
 w2_input = int(input("Add sales for last day of w2: "))
 sales_w2.append(w2_input)
-print(sales_w2)
+#print(sales_w2)
+sales = sales_w1 + sales_w2
+#print(sales)
+max_sales = max(sales) * 1.5
+min_sales = min(sales) * 1.5
+total_sales = min_sales + max_sales
+print(f"Best day earnings: {max_sales}$")
+print(f"Worst day earnings: {min_sales}$")
+print(f"Total earnings: {total_sales}$")
