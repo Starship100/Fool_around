ea8fcb08fa8ffe9717c59a69aefd950fdd638d7e
Starship100 <jimmy.widlund@outlook.com>
2026-02-15 16:16:46 +0100

    list_sales

---
+++ split_and_join.py
@@ -0,0 +1,10 @@
+
+msg = "Welcome to Python 101: Split and Join"
+csv = "Eric, John, Michael, Terry, Graham"
+friends_list = ["Eric", "John", "Michael", "Terry", "Graham"]
+print(list(msg.split() ))
+print(csv.split(";"))
+print("".join(csv))
+
+csv = "Eric, John, Michael, Terry, Graham"
+friends_list = ["Eric", "John", "Michael", "Terry", "Graham"]
