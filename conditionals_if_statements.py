7c77ccb487cd8fb0b91c1c19d8b285aed4a199ea
Starship100 <jimmy.widlund@outlook.com>
2026-02-16 12:19:06 +0100

    functions.py

--- conditionals_if_statements.py
+++ conditionals_if_statements.py
@@ -1,17 +1,18 @@
 
+operator = input("Pick operator +, -, *, / or f for Celsius to Fahrenheit: ")
 first_number = int(input("Add a number: "))
-second_number = int(input("Add a second number: "))
-operator = input("Pick operator +, -, *, /: ")
+if operator == "f":
+    print(f"{first_number} in Celsius is {first_number * 9 / 5 + 32} in Fahrenheit")
+else:
+    second_number = int(input("Add a second number: "))
 
-if operator == "+":
-    plus = first_number + second_number
-    print(plus)
-elif operator == "-":
-    plus = first_number - second_number
-    print(plus)
-elif operator == "*":
-    plus = first_number * second_number
-    print(plus)
-elif operator == "/":
-    plus = first_number / second_number
-    print(round(plus, 2))
+    if operator == "+":
+        print(f"Answer: {first_number + second_number}")
+    elif operator == "-":
+        print(f"Answer: {first_number - second_number}")
+    elif operator == "*":
+        print(f"Answer: {first_number * second_number}")
+    elif operator == "/":
+        print(f"Answer: {round(first_number / second_number, 2)}")
+    else:
+        print(">Input error")
