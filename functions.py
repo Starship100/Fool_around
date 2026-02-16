b0c23287c778db01f6c1d50785b3b4bc8f67f8f9
Starship100 <jimmy.widlund@outlook.com>
2026-02-16 09:02:23 +0100

    functions.py

--- functions.py
+++ functions.py
@@ -1,13 +1,14 @@
 
 def greeting(name , age=23, color="red"):
     print(f"Hello {name.capitalize()}, you will be {age+1} next year!")
+    print(f"We hear you like the color {color.lower()}!")
 
 def greeting_1(name, age, color):
     print(f"Hello {name.capitalize()}, you will be {age+1} next year!")
     print(f"We hear you like the color {color.lower()}!")
 
 # Uses greeting function with default values
-greeting("Dwight", 35)
+greeting("Dwight", 35, "Blue")
 greeting("pam")
 
 # Uses greeting_1 function where default values don't work
